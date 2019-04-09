4a11bf522b5dadbf4c7481110742394d5b14ca02
Sarah Anderson <saranderson13@gmail.com>
2019-04-09 04:07:21 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |style|
+    style[1].each do |language|
+      new_hash[language[0]] = language[1]
+      new_hash[language[0]][:style] = []
+    end
+  end
+  
+  languages.each do |style| 
+    style[1].each { |language| new_hash[language[0]][:style] << style[0] } 
+  end
+  
+  new_hash
 end
+
+
